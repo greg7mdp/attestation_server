beb6eca3f391279089fd3ecaa9a3dcd44612aa8b
greg7mdp <greg7mdp@gmail.com>
2022-04-29 18:16:09 -0400

    wip

---
+++ libxrpl/common.h
@@ -0,0 +1,10 @@
+#ifndef LIBXRPL_COMMON_H_INCLUDED
+#define LIBXRPL_COMMON_H_INCLUDED
+
+#include <cstdint>
+#include <string>
+#include <string_view>
+
+#include "libxrpl_export.h"
+
+#endif // LIBXRPL_COMMON_H_INCLUDED
