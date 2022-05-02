950e783774e70eb7c79cb231fea0c6d93cba7991
greg7mdp <greg7mdp@gmail.com>
2022-05-02 11:28:48 -0400

    wip

--- libxrpl/common.h
+++ libxrpl/common.h
@@ -7,4 +7,22 @@
 
 #include "libxrpl_export.h"
 
+namespace xrpl {
+    
+    using PublicKey = std::string;
+    using SecretKey = std::string;
+
+    struct Port
+    {
+        enum class Protocol { http, ws, peer };
+        
+        Protocol protocol;
+        size_t port_nb;
+        // ? ip;
+        // ? admin;
+    };
+        
+
+}
+
 #endif // LIBXRPL_COMMON_H_INCLUDED
