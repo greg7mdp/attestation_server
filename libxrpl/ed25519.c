9a92c9fe02892e5e95716207dabb1ea41c65473a
greg7mdp <greg7mdp@gmail.com>
2022-05-04 10:35:03 -0400

    wip

---
+++ libxrpl/ed25519.c
@@ -0,0 +1,19 @@
+#include "stdlib.h"
+#include "assert.h"
+
+typedef unsigned char ed25519_signature[64];
+typedef unsigned char ed25519_public_key[32];
+typedef unsigned char ed25519_secret_key[32];
+
+extern void ed25519_publickey(const ed25519_secret_key sk, ed25519_public_key pk)
+{
+    assert(0);
+    abort();
+}
+
+extern void ed25519_sign(const unsigned char *m, size_t mlen, const ed25519_secret_key sk,
+                         const ed25519_public_key pk, ed25519_signature RS)
+{
+    assert(0);
+    abort();
+}
