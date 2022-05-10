947f4fe082224bdd9a92d890e28e3e4a9774eb9f
greg7mdp <greg7mdp@gmail.com>
2022-05-10 13:12:55 -0400

    rgnfill fix

--- libxrpl/rngfill.h
+++ libxrpl/rngfill.h
@@ -12,24 +12,13 @@ template <class Generator>
 void rngfill(void* buffer, std::size_t bytes, Generator& g)
 {
     using result_type = typename Generator::result_type;
-    while (bytes >= sizeof(result_type))
+    while (bytes > 0)
     {
         auto const v = g();
         std::memcpy(buffer, &v, sizeof(v));
         buffer = reinterpret_cast<std::uint8_t*>(buffer) + sizeof(v);
-        bytes -= sizeof(v);
+        bytes -= std::min(bytes, sizeof(v));
     }
-#ifdef __GNUC__
-    // gcc 11.1 (falsely) warns about an array-bounds overflow in release mode.
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Warray-bounds"
-    if (bytes > 0)
-    {
-        auto const v = g();
-        std::memcpy(buffer, &v, bytes);
-    }
-#pragma GCC diagnostic pop
-#endif
 }
 
 template <class Generator, std::size_t N, class = std::enable_if_t<N % sizeof(typename Generator::result_type) == 0>>
